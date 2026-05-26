7bcbca9d5b69b53564d220a7d8130a61b0070e1e
Pedro Victor <143669078+pepe123uyt@users.noreply.github.com>
2026-05-26 13:23:25 +0000

    Terminei cercado

--- ED/ed/cercado/src/go/main.go
+++ ED/ed/cercado/src/go/main.go
@@ -9,6 +9,7 @@ import (
 type Pos struct {
 	r, c int
 }
+
 func dps(board [][]byte, visi map[Pos]bool, r, c int) {
 	if r < 0 || r >= len(board) || c < 0 || c >= len(board[0]) || board[r][c] == 'X' {
 		return
@@ -30,12 +31,32 @@ func dps(board [][]byte, visi map[Pos]bool, r, c int) {
 func solve(board [][]byte) {
 	visi := make(map[Pos]bool)
 
+	for c := range board[0] {
+		if board[0][c] == 'O' {
+			dps(board, visi, 0, c)
+		}
+		if board[len(board)-1][c] == 'O' {
+			dps(board, visi, len(board)-1, c)
+		}
+	}
+
+	for r := range board {
+		if board[r][0] == 'O' {
+			dps(board, visi, 0, r)
+		}
+		if board[r][len(board[0])-1] == 'O' {
+			dps(board,visi, r, len(board[0])-1)
+		}
+	}
+
 	for r := range board {
 		for c := range board[0]{
-			if board[r][c] == 'O' {
-				dps(board, visi, r, c)
+			pos := Pos{r,c}
+
+			if board[r][c] == 'O' && !visi[pos] {
+				board[r][c] = 'X'
 			}
-		}
+		} 
 	}
 }
 
