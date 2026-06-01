61ed4bb42a964e141ed34c419ed288c9a727a081
Pedro Victor <143669078+pepe123uyt@users.noreply.github.com>
2026-06-01 15:51:40 +0000

    fiz balanceada

---
+++ ED/ed/balanceada/src/go/draft.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Stack struct {
+    data []rune
+}
+
+func NewStack() *Stack{
+    return &Stack{data: []rune{}}
+}
+
+func (s *Stack) Push(value rune) {
+    s.data = append(s.data, value)
+}
+
+func (s *Stack) Top() rune {
+	if len(s.data) == 0 {
+		panic("stack is empty")
+	}
+	return s.data[len(s.data)-1]
+}
+
+func (s *Stack) Pop() {
+	if s.IsEmpty() {
+		panic("stack is empty")
+	}
+	s.data = s.data[:len(s.data)-1]
+}
+
+func (s *Stack) IsEmpty() bool {
+    if len(s.data) <= 0 {
+        return true
+    }
+    return false
+}
+
+func isBalance(formula []rune) bool {
+    if len(formula) % 2 != 0 {
+        return false
+    }
+
+    stack := NewStack()
+    
+    for _, i := range formula {
+        if i == '(' || i == '[' {
+            stack.Push(i)
+        }
+
+        if !stack.IsEmpty() {
+            s := stack.Top()
+    
+            if i == ')'{
+                if s == '(' {
+                    stack.Pop()
+                } else {
+                    return false
+                }
+            }
+            if i == ']' {
+                if s == '[' || s == ')' {
+                    stack.Pop()
+                } else {
+                    return false
+                }
+            }
+            
+        } else {
+            return false
+        }
+
+    }
+    return true
+}
+func main() {
+    scanner := bufio.NewScanner(os.Stdin)
+
+    scanner.Scan()
+    formula := []rune(scanner.Text())
+
+    if isBalance(formula) {
+        fmt.Println("balanceado")
+    } else {
+        fmt.Println("nao balanceado")
+    }
+}
