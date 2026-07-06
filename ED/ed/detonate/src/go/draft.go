6eb2551fd41535ab1e76ef52b12fb9d0fbcbb198
Pedro Victor <143669078+pepe123uyt@users.noreply.github.com>
2026-07-06 19:02:27 +0000

    fiz wordsearch

---
+++ ED/ed/detonate/src/go/draft.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+
+func main() {
+    var xi, yi int
+    fmt.Scan(&xi, &yi)
+}
--- ED/ed/wordsearch/src/go/main.go
+++ ED/ed/wordsearch/src/go/main.go
@@ -8,7 +8,60 @@ import (
 
 // Não mude a assinatura desta função, ela é a função chamada pelo LeetCode
 func exist(grid [][]byte, word string) bool {
-	_, _ = grid, word
+	n_linhas := len(grid)
+	n_colunas := len(grid[0])
+
+	for l := range n_linhas {
+		for c := range n_colunas {
+			if backtrack(grid, word, n_linhas, n_colunas, l, c) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func backtrack(grid [][]byte, suffix string, n_linhas, n_colunas, linha, coluna int) bool {
+	if len(suffix) == 0 {
+		return true
+	}
+
+	if linha < 0 || linha == n_linhas || coluna < 0 || coluna == n_colunas {
+		return false
+	}
+
+	if grid[linha][coluna] != suffix[0] {
+		return false
+	}
+
+	grid[linha][coluna] = '#'
+
+	directions := [][]int {
+		{0, 1},
+		{1, 0},
+		{0, -1},
+		{-1, 0},
+	}
+
+	for _, dir := range directions {
+		linhaOffset := dir[0]
+		colunaOffset := dir[1]
+
+		res := backtrack(
+			grid,
+			suffix[1:],
+			n_linhas,
+			n_colunas,
+			linha + linhaOffset,
+			coluna + colunaOffset,
+		)
+
+		if res {
+			return true
+		}
+	}
+
+	grid[linha][coluna] = suffix[0]
 	return false
 }
 
